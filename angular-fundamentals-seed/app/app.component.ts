27bd7e9365aaca034d83689293a0e610e9b5c328
Sunny Mistry <sunnymis@buffalo.edu>
2017-04-07 21:01:20 -0400

    ngFor ngIf

--- angular-fundamentals-seed/app/app.component.ts
+++ angular-fundamentals-seed/app/app.component.ts
@@ -1,23 +1,59 @@
 import { Component } from '@angular/core';
 
+interface Passenger {
+  id: number,
+  fullname: string,
+  checkedIn: boolean
+}
+
 @Component({
   selector: 'app-root',
   styleUrls: ['app.component.scss'],
   template: `
     <div class="app">
       <button (click)="handleClick(username.value)">Get Value</button>
-      <input type="text" #username />
+      <input type="text" 
+        #username 
+        (input)="handleInput($event.target.value)"
+        />
       <div>{{ title }}</div>
+      <div *ngIf="name.length > 3">
+        Searching for {{ name }}
+      </div>
+      <div>
+        <h3>Airline Passengers</h3>
+        <ul>
+          <li *ngFor="let passenger of passengers; let i = index;">
+            {{ i }}: {{passenger.fullname}}
+          </li>
+        </ul>
+      </div>
     </div>
   `
 })
 export class AppComponent {
   title: string
+  name: string = '';
+  passengers: Passenger[] = [{
+    id: 1,
+    fullname: 'Stephen',
+    checkedIn: true
+  }, {
+    id: 2,
+    fullname: 'Rose',
+    checkedIn: true
+  }, {
+    id: 3,
+    fullname: 'James',
+    checkedIn: false
+  }]
   constructor() {
     this.title = 'Ultimate Angular'
   }
   handleClick(value: any) {
     console.log(value);
   }
-
+  handleInput(value: any) {
+    this.name = value;
+  }
 }
