7cb5666ba352eb063a7a5c2e9bc660139855f19d
Sunny Mistry <sunnymis@buffalo.edu>
2017-04-09 13:03:39 -0400

    Child components count and detail

--- angular-fundamentals-seed/app/passenger-dashboard/passenger-dashboard.module.ts
+++ angular-fundamentals-seed/app/passenger-dashboard/passenger-dashboard.module.ts
@@ -1,11 +1,20 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+
+// containers
 import { PassengerDashboardComponent } from "./containers/passenger-dashboard/passenger-dashboard.component";
 
+//components
+import { PassengerCountComponent } from "./components/passenger-count/passenger-count.component";
+import { PassengerDetailComponent } from "./components/passenger-detail/passenger-detail.component";
 @NgModule({
     imports: [CommonModule],
     exports: [PassengerDashboardComponent],
-    declarations: [PassengerDashboardComponent],
+    declarations: [
+        PassengerDashboardComponent,
+        PassengerCountComponent,
+        PassengerDetailComponent
+    ],
     providers: [],
 })
 export class PassengerDashboardModule { }
