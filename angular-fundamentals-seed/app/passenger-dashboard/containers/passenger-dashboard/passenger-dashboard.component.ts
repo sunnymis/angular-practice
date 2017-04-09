c836b8fc3001b564a87ec84dc21bc1dc19b54016
Sunny Mistry <sunnymis@buffalo.edu>
2017-04-09 12:53:58 -0400

    ngOnInit

--- angular-fundamentals-seed/app/passenger-dashboard/containers/passenger-dashboard/passenger-dashboard.component.ts
+++ angular-fundamentals-seed/app/passenger-dashboard/containers/passenger-dashboard/passenger-dashboard.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Passenger } from "../../models/passenger.interface";
 
 @Component({
@@ -28,24 +28,29 @@ import { Passenger } from "../../models/passenger.interface";
     </div>
     `
 })
-export class PassengerDashboardComponent {
-    passengers: Passenger[] = [{
-        id: 1,
-        fullname: 'Stephen',
-        checkedIn: true,
-        checkInDate: 1490742000000,
-        children: [{ name: 'Ted', age: 12 }, { name: 'Chloe', age: 7 }]
-    }, {
-        id: 2,
-        fullname: 'Rose',
-        checkedIn: true,
-        checkInDate: 1491606000000,
-        children: null
-    }, {
-        id: 3,
-        fullname: 'James',
-        checkedIn: false,
-        checkInDate: null,
-        children: [{ name: 'Jessica', age: 1 }]
-    }]
+export class PassengerDashboardComponent implements OnInit {
+    passengers: Passenger[];
+    constructor() { }
+    ngOnInit() {
+        console.log('ngOnInit()')
+        this.passengers = [{
+            id: 1,
+            fullname: 'Stephen',
+            checkedIn: true,
+            checkInDate: 1490742000000,
+            children: [{ name: 'Ted', age: 12 }, { name: 'Chloe', age: 7 }]
+        }, {
+            id: 2,
+            fullname: 'Rose',
+            checkedIn: true,
+            checkInDate: 1491606000000,
+            children: null
+        }, {
+            id: 3,
+            fullname: 'James',
+            checkedIn: false,
+            checkInDate: null,
+            children: [{ name: 'Jessica', age: 1 }]
+        }]
+    }
 }
