fc15c41d8f8b6c900740bf224582c2ab31db928d
Sunny Mistry <sunnymis@buffalo.edu>
2017-04-10 19:55:57 -0400

    Injectabel

--- angular-fundamentals-seed/app/passenger-dashboard/passenger-dashboard.module.ts
+++ angular-fundamentals-seed/app/passenger-dashboard/passenger-dashboard.module.ts
@@ -1,5 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { HttpModule } from '@angular/http';
 
 // containers
 import { PassengerDashboardComponent } from "./containers/passenger-dashboard/passenger-dashboard.component";
@@ -11,7 +12,10 @@ import { PassengerDetailComponent } from "./components/passenger-detail/passenge
 import { PassengerDashboardService } from "./passenger-dashboard.service";
 
 @NgModule({
-    imports: [CommonModule],
+    imports: [
+        CommonModule,
+        HttpModule
+    ],
     exports: [PassengerDashboardComponent],
     declarations: [
         PassengerDashboardComponent,
--- angular-fundamentals-seed/app/passenger-dashboard/passenger-dashboard.service.ts
+++ angular-fundamentals-seed/app/passenger-dashboard/passenger-dashboard.service.ts
@@ -1,7 +1,10 @@
+import { Injectable } from '@angular/core';
+import { Http } from '@angular/http';
 import { Passenger } from "./models/passenger.interface";
 
+@Injectable()
 export class PassengerDashboardService {
-    constructor() { }
+    constructor(private http: Http) { console.log(http) }
 
     getPassengers(): Passenger[] {
         return [{
